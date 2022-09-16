73ea3e85a90f215cfee00e54ad416772886cdcd2
Mik <info@slnt-opp.xyz>
2022-09-16 18:36:30 +0200

    German plates generator

---
+++ pkg/plates/de.go
@@ -0,0 +1,25 @@
+package plates
+
+import (
+	"math/rand"
+
+	wr "github.com/mroth/weightedrand"
+)
+
+var DE_REGION_CHOOSER, _ = wr.NewChooser(
+	wr.Choice[int]{Item: 1, Weight: 60},
+	wr.Choice[int]{Item: 2, Weight: 35},
+	wr.Choice[int]{Item: 3, Weight: 5},
+)
+
+func DE() Plate {
+	l := rand.Intn(3) + 6
+	number := String(DE_REGION_CHOOSER.Pick(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	number += " "
+	number += String(l-len(number), LETTERS)
+
+	return Plate{
+		Country: "D",
+		Number:  number,
+	}
+}
